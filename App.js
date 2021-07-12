d52b9e1a3aced4a548344ca7e7bb69e41a317da6
jainsahab-1506 <2019UCP1390@mnit.ac.in>
2021-07-12 19:28:37 +0530

    initial

---
+++ App.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.json());
+mongoose.connect(
+  "mongodb+srv://admin-naman:" +
+    process.env.CLUSTER_PASSWORD +
+    "@cluster0.3djy5.mongodb.net/MusicPortalDB?retryWrites=true&w=majority",
+  { useNewUrlParser: true },
+  () => {
+    console.log("Database connected.");
+  }
+);
+
+app.get("/", function (req, res) {
+  res.send("Hello");
+});
+app.listen(3000, function (req, res) {
+  console.log("Running");
+});
